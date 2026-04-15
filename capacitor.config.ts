1b4b1eda70d1efb2ff8ddaa67e06c8bbdc470281
simonjhuan <49852845+simonjhuan@users.noreply.github.com>
2026-04-15 14:51:49 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.goodfile.app',
+  appName: 'goodfile',
+  webDir: 'www'
+};
+
+export default config;
